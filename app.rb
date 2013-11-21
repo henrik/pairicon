0f6ff2999542678fe817bba834d9961b8f0313fe
Henrik Nyh <henrik@nyh.se>
2013-11-21 22:28:46 +0100

    Can provide by URL. Handle single name submission.

--- app.rb
+++ app.rb
@@ -10,8 +10,16 @@ TITLE = "Pairicon"
 MAX = 4
 
 get "/" do
+  with_names params[:name]
+end
+
+get %r{/(\w+(?:\+\w+)+)} do |names|
+  with_names names.split("+")
+end
+
+def with_names(names)
   @title = TITLE
-  @names = Array(params[:name]).reject(&:empty?).first(MAX)
-  @url = Icon.new(*@names).url if @names.any?
+  @names = Array(names).reject(&:empty?).first(MAX)
+  @url = Icon.new(*@names).url if @names.length > 1
   slim :index
 end
