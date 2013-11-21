567be99df309c8ddc453a580aa83eda874e2c739
Henrik Nyh <henrik@nyh.se>
2013-11-21 22:55:28 +0100

    On unknown user, use default avatar instead of breaking.

--- icon.rb
+++ icon.rb
@@ -5,6 +5,10 @@ class Icon
   USER_AGENT = "pairicon.herokuapp.com"
   GITHUB_TOKEN = ENV["GITHUB_TOKEN"]
 
+  # This one is Cloudinary's.
+  # Some default image is better than the image being entirely broken.
+  DEFAULT_GRAVATAR_ID = "e3264cf16f34ecd3c7c564f5668cbc1e"
+
   POSITIONS = {
     2 => [[0, 0], [60, 60]],
     3 => [[0, 0], [80, 0], [40, 80]],
@@ -35,7 +39,7 @@ class Icon
     data["gravatar_id"]
   rescue OpenURI::HTTPError => e
     if e.message.include?("404 Not Found")
-      "fakehash"
+      DEFAULT_GRAVATAR_ID
     else
       raise
     end
