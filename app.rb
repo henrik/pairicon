235d6b9a24a4298d5052b7eedfd4d6b7a501b04e
Henrik Nyh <henrik@nyh.se>
2013-11-21 23:15:31 +0100

    /foo+bar.png for an image (redirect).

--- app.rb
+++ app.rb
@@ -10,16 +10,34 @@ TITLE = "Pairicon"
 MAX = 4
 
 get "/" do
-  with_names params[:name]
+  render_with_names params[:name]
+end
+
+get %r{/(\w+(?:\+\w+)+)\.png\z} do |names|
+  names = clean_names(names.split("+"))
+  url = icon_url_from_names(names)
+  redirect to(url)
 end
 
 get %r{/(\w+(?:\+\w+)+)} do |names|
-  with_names names.split("+")
+  render_with_names names.split("+")
 end
 
-def with_names(names)
+def render_with_names(names)
   @title = TITLE
-  @names = Array(names).reject(&:empty?).first(MAX)
-  @url = Icon.new(*@names).url if @names.length > 1
+  @names = clean_names(names)
+  @url = icon_url_from_names(@names)
   slim :index
+end
+
+def icon_url_from_names(names)
+  if names.length > 1
+    Icon.new(*names).url
+  else
+    nil
+  end
+end
+
+def clean_names(raw_names)
+  Array(raw_names).reject(&:empty?).first(MAX)
 end
